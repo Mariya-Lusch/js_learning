d8e7647e3174376592c01368fa83fc89a8775ca1
Mariya-Lusch <ms.luschik@gmail.com>
2023-07-08 17:55:51 +0300

    add if

--- js.js
+++ js.js
@@ -3,6 +3,23 @@ const secondName = prompt("Введите имя партнера");
 
 const randomNumder = Math.floor(Math.random() * 100);
 
-alert(
-  `Результат совестимости ${firstName} и ${secondName} равен ${randomNumder}`
-);
+if (randomNumder < 33) {
+  alert(
+    `Результат совестимости ${firstName} и ${secondName} равен ${randomNumder}
+    Рекомендуем поискать другой вариант...`
+  );
+} else if (randomNumder >= 33 && randomNumder < 66) {
+  alert(
+    `Результат совестимости ${firstName} и ${secondName} равен ${randomNumder}
+    Неплохо, если поработать над отношениями...`
+  );
+} else if (randomNumder >= 66) {
+  alert(
+    `Результат совестимости ${firstName} и ${secondName} равен ${randomNumder}
+    Отличная совместимость! Но помни, над отношениями всегда нужно работать...`
+  );
+} else {
+  alert(
+    "Упс... Что-то пошло не так... Попробуй перезапустить страницу и заново ввести имена"
+  );
+}
