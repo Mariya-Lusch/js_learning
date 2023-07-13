5202b8d8abd5bffe545e70daf7a88681b9f782e9
Mariya-Lusch <ms.luschik@gmail.com>
2023-07-13 16:41:13 +0300

    arrow function

--- file2.js
+++ file2.js
@@ -1,8 +1,6 @@
-// Функции колбэка
+// Стрелочные функции
 
 "use strict";
-
-//function decloration
 
 function ask(question, yes, no) {
   if (confirm(question)) {
@@ -10,23 +8,12 @@ function ask(question, yes, no) {
   } else no();
 }
 
-function showOk() {
-  alert("Спасибо за согласие");
-}
-
-function showCansel() {
-  alert("Вы отказались");
-}
-
-// ask("Вы согласны?", showOk, showCansel);
-
-//function expresion
 ask(
   "Вы согласны?",
-  function () {
-    alert("Спасибо за согласие");
-  },
-  function () {
-    alert("Вы отказались");
-  }
-); // у функции убираем название, т.к. в данном случае оно нам не нужно, обращаться к нему мы не будем
+  () => alert("Спасибо за согласие"),
+  () => alert("Вы отказались")
+);
+
+let sum = (a, b) => a + b;
+
+console.log(sum(5, 5));
