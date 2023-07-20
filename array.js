a16daaf81ed7928e75034656cf39f9b777ab346c
Mariya-Lusch <ms.luschik@gmail.com>
2023-07-20 20:54:21 +0300

    add and remove method

--- array.js
+++ array.js
@@ -1,37 +1,42 @@
 // МАССИВЫ лекция
 
-const userArr = ["Mary", 28];
+// 1. добавление в конец массива
+const userArr = ["Mary", 28, 30];
+userArr.push("female");
 console.log(userArr);
 
-// 1. вывести только второй элемент массива
-console.log(userArr[1]);
+// 2. Добавление в начало массива
+const userArrOne = ["Mary", 28, 30];
+userArrOne.unshift("Russian");
+console.log(userArrOne);
 
-// 2. добавить элемент
-userArr[100] = "Luschik";
-console.log(userArr);
-// создано 97 пустых ячеек, чтобы новое значение имело индекс 100. при этом, строк будет 101.
+// 3. Удаление в конце массива
+const userArrTwo = ["Mary", 28, 30];
+userArrTwo.pop();
+console.log(userArrTwo);
 
-// 3. изменение элемента массива
-userArr[100] = "Kim";
-console.log(userArr);
+// 4. Удаление в начале массива
+const userArrThree = ["Mary", 28, 30];
+userArrThree.shift();
+console.log(userArrThree);
 
-// 4. В массив можно записывать различные типы данных, например, функцию
-// const userArrOne = [
-//   "Mary",
-//   28,
-//   30,
-//   function () {
-//     alert("Hello, my friends");
-//   },
-// ];
-// userArrOne[3]();
-// console.log(userArrOne);
+// 5. Удаление элемента/элементов внутри массива
+const userArrFour = ["Mary", 28, 30, 2023 - 1995, "Russian", "female"];
+userArrFour.splice(2, 1);
+console.log(userArrFour);
 
-// 5. В массивы можно записывать выражения
-const userArrTwo = ["Mary", 28, 30, 40, 2000 - 1995];
-console.log(userArr);
+// 6. изменение элементов с помощью splice
+userArrFour.splice(2, 1, "Lusch", 3458);
+console.log(userArrFour);
 
-// 6. Вывести значение последнего индекса, нпр, если их очень много в массиве
-console.log(userArrTwo[userArrTwo.length - 1]);
-// но эту запись можно скоратить, используя метод массива at
-console.log(userArrTwo.at(-1));
+// Метод splice возвращает удаленные элементы в виде массивов.
+const userArrFive = ["Mary", 28, 30, "Lusch", "Russian", "female", "cat"];
+const dataFromUser = userArrFive.splice(1, 3);
+console.log(userArrFive);
+console.log(dataFromUser);
+
+// проверка на наличие того или иного элемента в массиве - возвращает булевое значение
+console.log(userArrFive.includes("Mary"));
+
+// проверка наличие опреденного элемента и возвращение его индекса
+console.log(userArrFive.indexOf("Lusch"));
