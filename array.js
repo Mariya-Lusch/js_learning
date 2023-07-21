a5bd69a8d7617968165d3d26c82bfa87ab995c31
Mariya-Lusch <ms.luschik@gmail.com>
2023-07-21 15:08:21 +0300

    for continue / break

--- array.js
+++ array.js
@@ -1,11 +1,43 @@
-// МАССИВЫ лекция
+// МАССИВЫ лекция.
 
-for (let i = 1; i <= 15; i++) {
-  console.log("Это сообщение выведется 15 раз");
+const arr = [
+  "Mariya",
+  "Luschik",
+  2023 - 1995,
+  "Pushkino",
+  true,
+  ["mother", "father", "brother", "husb"],
+];
+
+// console.log(arr);
+
+// вывести каждый элемент массива по отдельности
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
 
-for (let i = 1; i <= 15; i++) {
-  if (i > 1 && i <= 4) {
-    console.log(`Это сообщение выведется ${i} раза`);
-  } else console.log(`Это сообщение выведется ${i} раз`);
+// вывести тип данных
+for (let i = 0; i < arr.length; i++) {
+  console.log(typeof arr[i]);
+}
+
+// поместить конвертиврованные данные в другой массив
+const arr2 = [];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(typeof arr[i]);
+  arr2.push(typeof arr[i]);
+}
+console.log(arr2);
+
+// выборка опреденного типа данных из массива - пропуск итерации с помощью continue
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] !== "string") continue;
+  console.log(arr[i]);
+}
+
+// прекращение выполнения цикла с помощью break
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === "number") break;
+  console.log(arr[i]);
 }
